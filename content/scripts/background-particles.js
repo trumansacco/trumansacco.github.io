29bfa894241a840eee288e9219402332eb573b90
trumansacco <156451821+trumansacco@users.noreply.github.com>
2024-01-14 15:28:59 -0600

    fix

--- content/scripts/background-particles.js
+++ content/scripts/background-particles.js
@@ -1,4 +1,3 @@
-<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
   
     document.addEventListener('DOMContentLoaded', function () {
       particlesJS('particles-js', {
