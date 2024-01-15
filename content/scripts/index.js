b24e3190f68e68af99ff8c5662a7fd6d842ebc37
trumansacco <156451821+trumansacco@users.noreply.github.com>
2024-01-15 10:14:17 -0600

    Update index.js

--- content/scripts/index.js
+++ content/scripts/index.js
@@ -2,27 +2,39 @@ document.addEventListener("DOMContentLoaded", function () {
   function handleMouseMove(event) {
     const navbar = document.getElementById('navbar');
     const mouseY = event.clientY;
+    const mouseYVH = (mouseY / window.innerHeight) * 100;
 
-    if (mouseY <= 100) {
-      navbar.classList.add('show');
-    } else {
+    const iframe = document.querySelector('iframe');
+    const iframeRect = iframe.getBoundingClientRect();
+    const isMouseOverIframe = (
+      event.clientX >= iframeRect.left &&
+      event.clientX <= iframeRect.right &&
+      event.clientY >= iframeRect.top &&
+      event.clientY <= iframeRect.bottom
+    );
+
+    if (mouseYVH > 100 || isMouseOverIframe) {
       navbar.classList.remove('show');
+    } else {
+      navbar.classList.add('show');
     }
   }
 
   document.addEventListener('mousemove', handleMouseMove);
 
-  const particlesElement = document.getElementById('particles-js');
-
   document.body.addEventListener('click', function (event) {
+    const particlesElement = document.getElementById('particles-js');
+
+    // Create a click event with mouse coordinates
     const clickEvent = new MouseEvent('click', {
       bubbles: true,
       cancelable: true,
       view: window,
-      clientX: event.clientX, // Capture mouse X coordinate
-      clientY: event.clientY, // Capture mouse Y coordinate
+      clientX: event.clientX,
+      clientY: event.clientY,
     });
 
+    // Dispatch the click event to particles-js
     particlesElement.dispatchEvent(clickEvent);
   });
 
